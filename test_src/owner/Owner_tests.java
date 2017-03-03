97603e19171eff62bae5c81a64cd4c1c5900c1df
Liam <s3606826@student.rmit.edu.au>
2017-03-03 14:39:14 +1100

    Create Owner_test.java

---
+++ test_src/owner/Owner_tests.java
@@ -0,0 +1,16 @@
+package owner;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Owner_tests
+{
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
