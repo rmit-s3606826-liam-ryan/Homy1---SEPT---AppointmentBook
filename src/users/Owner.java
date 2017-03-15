08544b0b68ca86b03f0cb97f3cc9e4232b1b8fcb
Nhat Ho <s3541433@student.rmit.edu.au>
2017-03-15 11:03:40 +1100

    very basic owner class

--- src/users/Owner.java
+++ src/users/Owner.java
@@ -2,5 +2,12 @@ package users;
 
 public class Owner
 {
-
+	private String name;
+	private String passwords;
+	
+	public Owner(String name, String password)
+	{
+		this.name = name;
+		this.passwords = password;
+	}
 }
