4516bf560eb5288bf960eeab4364b92709fb4f66
Adam <s3284493@student.rmit.edu.au>
2017-04-10 03:58:00 +1000

    Employee Availability
    -Database availability table added
    -viewEmployeeAvailiability() implemented
    -Employees now store availability information, see Employee.java for
    details

--- src/users/Employee.java
+++ src/users/Employee.java
@@ -1,14 +1,13 @@
 package users;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import bookings.Timeslot;
+import java.time.LocalTime;
+import java.util.HashMap;
 
 public class Employee
 {
     private int id;
     private String name;
+    private HashMap<String, LocalTime[]> availability = new HashMap<String, LocalTime[]>();
 
     public Employee(int id, String name)
     {
@@ -25,5 +24,15 @@ public class Employee
     {
         return name;
     }
-
+    
+    public HashMap<String, LocalTime[]> getAvailability()
+    {
+    	return availability;
+    }
+    
+    public void addAvailability(String dayOfWeek, LocalTime start, LocalTime finish)
+    {
+    	LocalTime[] times = {start, finish};
+    	availability.put(dayOfWeek, times);
+    }
 }
